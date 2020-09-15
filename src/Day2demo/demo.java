98e49c758c303df21b236e69af00596c34941515
amit <amit.luthra@gmail.com>
2020-09-15 13:43:13 +0530

    new

---
+++ src/Day2demo/demo.java
@@ -0,0 +1,19 @@
+package Day2demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "lib\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://corporate.olacabs.com/");
+		System.out.println("Hello world");
+		
+
+	}
+
+}
